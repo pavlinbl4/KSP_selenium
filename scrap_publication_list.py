dd4eab36a31a0f25d5e730b93ed939531b2a469e
evgeniy <pavlinb4@gmail.com>
2022-08-31 10:01:11 +0300

    add name of published image

--- scrap_publication_list.py
+++ scrap_publication_list.py
@@ -4,12 +4,14 @@ get real publication for the image KP archive
 
 from bs4 import BeautifulSoup
 
-publication_page = '/Users/evgeniy/Documents/Kommersant/Kommersant Photo Archive История публикаций.html'
+publication_page = '/Users/evgeniy/Desktop/test_page/Kommersant Photo Archive История публикаций_2.html'
 
-with open(publication_page, 'r') as file:
+with open(publication_page, 'r') as file:  # read offline  html page
     html = file.read()
 
 soup = BeautifulSoup(html, 'lxml')
+
+print(soup.find('h3').text)  # head with photo name
 
 main_table = soup.find(id='Table1').find('tbody')
 
@@ -24,7 +26,6 @@ publication_voc = {}
 count = 1
 for table in work_tables: # make same operation for two different tables
     for i in table:
-        # print(i.find_all('td')[0].find('img'))
         pub_check = i.find_all('td')[0].find('img')
         if pub_check is not None:
             if pub_check.get('src').split('/')[-1] == 'yes.gif':
@@ -33,18 +34,6 @@ for table in work_tables: # make same operation for two different tables
                 material = i.find_all('td')[4].text.strip()
                 publication_voc[count] = [publication_date,publication_place, material]
                 count += 1
-    print()
-
-
-
-
-
-# for i in main_table_electron:
-#     if i.find_all('td')[0].find('img') is not None and i.find_all('td')[0].find('img').get('src').split('/')[-1] != 'cancel.gif':
-#         publication_date = i.find_all('td')[1].text[:10]
-#         publication_place = i.find_all('td')[2].text.strip()
-#         material = i.find_all('td')[4].text.strip()
-#         publication_voc[publication_date] = [publication_place, material]
 
 for _ in publication_voc.items():
     print(_)
