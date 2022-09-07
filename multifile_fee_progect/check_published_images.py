b3d75166ca7a97b60f9c1fdbb6cd768a42fd8700
evgeniy <pavlinb4@gmail.com>
2022-09-07 09:57:52 +0300

    на даный момент работает без ошибок, но в отчете нет данных по сициальным сетям

--- multifile_fee_progect/check_published_images.py
+++ multifile_fee_progect/check_published_images.py
@@ -1,31 +1,35 @@
-from published_images import publication_info
-from write_xlsx import write_to_file
+from published_images import publication_info, published_for_all_time
+from write_xlsx import write_to_file,  write_xlsx_single_sheet
+from published_images import autorization, select_today_published_images, end_selenium
 
 red = '\033[91m'
 green = '\33[32m'
 end = '\033[0m'
 
 
-def images_in_cycle(images_voc, path_to_file,check_date):
+def images_in_cycle(images_voc, path_to_file, check_date):
     count = 0
     print(images_voc)
     for k in images_voc:
         count += 1
-        image_info = publication_info(k, count,check_date)
+        image_info = publication_info(k, count, check_date)
         write_to_file(path_to_file, image_info, count, check_date)
 
-def one_day_images_cycle(images_voc,i):
+
+def one_day_images_cycle(images_voc, i, path_to_file, count):
     print(f"{red} in  {i} - {len(images_voc)} were send to publication{end}")
-    print(f'{images_voc = }')
-    count = 0
+    # count = 0
     for k in images_voc:
         count += 1
-        image_info = publication_info(k, count,i)
-        
-images_voc = {'3265721': 'KSP_016797_00079', '3630967': 'KMO_189359_00044', '3631012': 'KMO_189359_00123', '3631017': 'KMO_189359_00049', '3631016': 'KMO_189359_00162', '3631018': 'KMO_189359_00065', '3630448': 'KSP_017660_00001'}
-
-i = 'source_page_03.08.2022.html'
-one_day_images_cycle(images_voc,i)
+        used_images = publication_info(k, count, i)
+        # write_to_file(path_to_file, used_images, count, i)
+        write_xlsx_single_sheet(path_to_file, used_images)
+    return count
 
 
-
+def image_id_cycle(images_voc):
+    count = 0
+    for k in images_voc:  # на самом деле снимок один и цикл не нужен - переделать
+        count += 1
+        image_info = published_for_all_time(k)
+    return image_info
