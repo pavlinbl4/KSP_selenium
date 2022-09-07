adcca170ac45e81166b8f7f800740f279d0d6a5f
evgeniy <pavlinb4@gmail.com>
2022-09-07 09:58:30 +0300

    на даный момент работает без ошибок, но в отчете нет данных по сициальным сетям

--- multifile_fee_progect/main_check_last_month.py
+++ multifile_fee_progect/main_check_last_month.py
@@ -1,23 +1,23 @@
 """
 1. в фотоархивве можно отсмотреть только историю "засыла" изображений
-2. "засланное" изображение не всегда опубликовано, нужно проверть это отдельно
+2. "засланное" изображение не всегда опубликовано, нужно проверить это отдельно
 """
 from kommersant_dates import KommersantDates
-from published_images import autorization, select_today_published_images, end
+from published_images import autorization, end_selenium
 from aditional_scripts.user_home_folder import home
 from check_published_images import one_day_images_cycle
-from cycle_for_html_save import month_cycle
 from images_links import get_image_links
 from images_vocabulary import make_images_voc
-
+import re
 import os
-from pathlib import Path
+from create_report_file import create_report_file
 
 # 1. Нужны данные по предыдущему месяцу
 
 kd = KommersantDates()  # вводя число получаю сдвиг с последнего дня месяца на указанное значение
-check_date = kd.previous_month_check_day  # по умолчанию это последний день месяца
+check_date = kd.previous_month_check_day  # по умолчанию это последний день предыдущего месяца
 days_in_month = kd.days_in_month  # количество дней в месяце
+path_to_file = create_report_file(kd.previous_month_name)
 
 # 2. нужно пройтись по всем дням месяца и получить данные о "засланных" снимках
 
@@ -45,14 +45,15 @@ autorization()
 # end()
 
 # 4 перебираю сохраненные страницы
-
+count = 0 # счетчик опубликованнеых снимков за весь месяц
 list_of_html = os.listdir(html_folder)
-for i in  list_of_html:
+for i in list_of_html:
     with open(f'{html_folder}/{i}', 'r') as file:
         html = file.read()
     images_links = get_image_links(html)  # список ссылок на "засланные" снимки
     images_voc = make_images_voc(images_links)  # словарь из "внутреннего" id снимка и стандартного, внешного  id
-    one_day_images_cycle(images_voc, i)
+    count = one_day_images_cycle(images_voc, re.findall(r'\d{2}.\d{2}.\d{4}',i)[0], path_to_file, count)
+    print(f'{i} - {count}')
 
 
-end()
+end_selenium()
