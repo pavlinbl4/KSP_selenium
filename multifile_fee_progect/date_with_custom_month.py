ef6660b401d47128d3036d9b0f546cc101d3bf4a
evgeniy <pavlinb4@gmail.com>
2022-09-08 17:01:34 +0300

    раотает, но в файле отчета нет социальных сетей

---
+++ multifile_fee_progect/date_with_custom_month.py
@@ -0,0 +1,14 @@
+import calendar
+from datetime import datetime
+from calendar import monthrange
+
+
+def custom_month_date(month_number):
+    current_year = datetime.now().year
+    end_month = monthrange(current_year, month_number)[1]
+    months_name = calendar.month_name[month_number]
+    check_date = datetime(current_year, month_number, end_month).strftime("%d.%m.%Y")
+
+    return months_name, check_date, end_month, current_year
+
+
