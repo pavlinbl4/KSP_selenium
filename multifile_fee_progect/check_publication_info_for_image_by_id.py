d46333ecbb374285b40d232652015629932e49c6
evgeniy <pavlinbl4@gmail.com>
2023-02-06 09:54:12 +0300

    publication history for image. Function take image id

--- multifile_fee_progect/check_publication_info_for_image_by_id.py
+++ multifile_fee_progect/check_publication_info_for_image_by_id.py
@@ -9,15 +9,19 @@ from images_links import get_image_links
 from images_vocabulary import make_images_voc
 from check_published_images import image_id_cycle
 
-image_id = 'KSP_016797_00019_1'
 
-autorization()
-html = check_id_image(image_id)
-images_links = get_image_links(html)  # список ссылок на "засланные" снимки
-images_voc = make_images_voc(images_links)  # словарь с внутренним id снимка и внешним id
-rezult = image_id_cycle(images_voc)
+def image_publication_history(image_id):
+    autorization()
+    html = check_id_image(image_id)
+    images_links = get_image_links(html)  # список ссылок на "засланные" снимки
+    images_voc = make_images_voc(images_links)  # словарь с внутренним id снимка и внешним id
+    rezult = image_id_cycle(images_voc)
 
-for i in rezult:
-    print(rezult[i])
+    for i in rezult:
+        print(rezult[i])
 
-end_selenium()
+    end_selenium()
+
+
+if __name__ == '__main__':
+    image_publication_history('KSP_016797_00019_1')
