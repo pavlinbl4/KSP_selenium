0dcd66169f49797a6c8f57fa463201ea025c62fe
evgeniy <pavlinbl4@gmail.com>
2023-02-07 16:53:48 +0300

    get information about images that were sent to publications

--- multifile_fee_progect/main_check_custom_month.py
+++ multifile_fee_progect/main_check_custom_month.py
@@ -2,7 +2,7 @@
 1. в фотоархивве можно отсмотреть только историю "засыла" изображений
 2. "засланное" изображение не всегда опубликовано, нужно проверить это отдельно
 """
-from kommersant_dates import KommersantDates
+
 from published_images import autorization, end_selenium, select_today_published_images
 from aditional_scripts.user_home_folder import home
 from check_published_images import one_day_images_cycle
@@ -11,19 +11,11 @@ from images_vocabulary import make_images_voc
 import re
 import os
 from create_report_file import create_report_file
-import calendar
 from datetime import datetime
-from calendar import monthrange
+from date_with_custom_month import custom_month_date
 
-month_number = 6  # int(input('input months number'))
-
-current_year = datetime.now().year
-end_month = monthrange(current_year, month_number)[1]
-months_name = calendar.month_name[month_number]
-check_date = datetime(current_year, month_number, end_month).strftime("%d.%m.%Y")
-days_in_month = end_month
-
-path_to_file = create_report_file(months_name)
+month_number = 1  # int(input('input months number'))
+months_name, check_date, days_in_month, current_year = custom_month_date(month_number)
 
 # 2. нужно пройтись по всем дням месяца и получить данные о "засланных" снимках
 
@@ -32,32 +24,27 @@ path_to_file = create_report_file(months_name)
 html_folder = home.add_subfolder_to_kommersant(f'test_{months_name}/HTML')
 os.makedirs(html_folder, exist_ok=True)
 
+path_to_file = create_report_file(months_name, html_folder)
+
 # 2.2  авторизируюсь на сайте
 autorization()
 
 # 3 на данном этапе сохраню все страницы для последующего анализа
 for day in range(1, days_in_month + 1):
     check_date = datetime(current_year, month_number, day).strftime("%d.%m.%Y")
-
     html = select_today_published_images(check_date)
-
     with open(f'{html_folder}/source_page_{check_date}.html', 'w') as file:
         file.write(html)
 
-# заменяю на функцию
-# month_cycle(days_in_month, html_folder)
-
-# end()
-
 # 4 перебираю сохраненные страницы
-# count = 0  # счетчик опубликованнеых снимков за весь месяц
-# list_of_html = os.listdir(html_folder)
-# for i in list_of_html:
-#     with open(f'{html_folder}/{i}', 'r') as file:
-#         html = file.read()
-#     images_links = get_image_links(html)  # список ссылок на "засланные" снимки
-#     images_voc = make_images_voc(images_links)  # словарь из "внутреннего" id снимка и стандартного, внешного  id
-#     count = one_day_images_cycle(images_voc, re.findall(r'\d{2}.\d{2}.\d{4}', i)[0], path_to_file, count)
-#     print(f'{i} - {count}')
+count = 0  # счетчик засланных снимков за весь месяц
+list_of_html = os.listdir(html_folder)
+for i in list_of_html:
+    with open(f'{html_folder}/{i}', 'r') as file:
+        html = file.read()
+    images_links = get_image_links(html)  # список ссылок на "засланные" снимки в течении одного дня
+    images_voc = make_images_voc(images_links)  # словарь из "внутреннего" id снимка и стандартного, внешнего id
+    count = one_day_images_cycle(images_voc, re.findall(r'\d{2}.\d{2}.\d{4}', i)[0], path_to_file, count)
+    print(f'{i} - {count = }')
 
 end_selenium()
