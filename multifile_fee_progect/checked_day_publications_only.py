a782a16d53f9bc4c4ab94b60ac675bb30907e7b3
evgeniy <pavlinbl4@gmail.com>
2023-03-07 18:53:45 +0300

    1

--- multifile_fee_progect/checked_day_publications_only.py
+++ multifile_fee_progect/checked_day_publications_only.py
@@ -8,7 +8,7 @@ green = '\33[32m'
 end = '\033[0m'
 
 
-def checked_day_publications_only(check_date,publication_voc, count):
+def checked_day_publications_only(check_date, publication_voc, count):
     image_info = {}
     for i in publication_voc:
         if publication_voc[i][0] == check_date:
@@ -19,13 +19,20 @@ def checked_day_publications_only(check_date,publication_voc, count):
             image_info["E"] = publication_voc[i][2]
     return image_info
 
-def checked_month_publications_only(check_date,publication_voc, count):
-    used_images = {}
+
+def checked_month_publications_only(check_date, publication_voc, count):
+    image_info = {}
     for i in publication_voc:
+        if publication_voc[i][1] == 'Соцсети (Коммерсантъ-Москва)' and publication_voc[i][3][3:] == check_date[3:]:
+            image_info['A'] = count
+            image_info['B'] = publication_voc[0]
+            image_info['C'] = publication_voc[i][3]
+            image_info["D"] = publication_voc[i][1]
+
         if publication_voc[i][0][3:] == check_date[3:]:
-            used_images['A'] = count
-            used_images['B'] = publication_voc[0]
-            used_images["C"] = publication_voc[i][0]
-            used_images["D"] = publication_voc[i][1]
-            used_images["E"] = publication_voc[i][2]
-    return used_images
+            image_info['A'] = count
+            image_info['B'] = publication_voc[0]
+            image_info["C"] = publication_voc[i][0]
+            image_info["D"] = publication_voc[i][1]
+            image_info["E"] = publication_voc[i][2]
+    return image_info
